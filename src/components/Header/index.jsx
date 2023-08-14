7b4456565a692e2ddff707716dcfb087ee36ef68
unknown <brunna@hretart.com.br>
2023-08-14 03:12:17 -0300

    feat: header componentizado

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import senFinanceLogo from "../../assets/images/senfinancelogo.png";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={senFinanceLogo} alt="SenFinance Logo" />
+    </header>
+  );
+}
